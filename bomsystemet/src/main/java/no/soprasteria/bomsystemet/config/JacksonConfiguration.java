39b214a4691a1acdc3b8624f91d16fd25d4b142e
Espen Waaga <espen.waaga@nav.no>
2022-01-05 10:57:22 +0100

    Legger til Objectmapper fra felles

---
+++ bomsystemet/src/main/java/no/soprasteria/bomsystemet/config/JacksonConfiguration.java
@@ -0,0 +1,17 @@
+package no.soprasteria.bomsystemet.config;
+
+import static no.soprasteria.felles.http.JacksonObjectMapper.mapper;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JacksonConfiguration {
+
+    @Bean
+    @Primary
+    public ObjectMapper customObjectmapper() {
+        return mapper;
+    }
+}
